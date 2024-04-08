ad4e7849eb5c0271ef27de33d8287241c9c26202
Erthium <ertyum35@gmail.com>
2024-04-08 04:00:40 +0300

    fix: draw the circles properly

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,11 +1,15 @@
 #include "simulation.hpp"
 
 void render_circles(sf::RenderWindow &window, vector<Circle> &circles, Master master) {
-  for (Circle &circle : circles) {
-    sf::CircleShape shape(circle.r);
-    shape.setPosition(circle.x, circle.y);
+  for (size_t i = 0; i < circles.size(); i++) {
+    sf::CircleShape shape(circles[i].r);
+    int x = circles[i].x - circles[i].r;
+    int y = circles[i].y - circles[i].r;
+    shape.setPosition(x, y);
     shape.setFillColor(sf::Color::White);
     window.draw(shape);
+    // write the index of the circle in the middle
+    // render_text(window, to_string(i), circles[i].x, circles[i].y, 10, sf::Color::Black);
   }
 }
 
